be2c80e57019dab2643b6dad3ee95ae9e5d2b811
DwayneJdev <dwaynej14@gmail.com>
2022-02-12 14:20:02 -0500

    stuck on user role for login

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import Signup from './components/Signup';
+
 
 function App() {
+  const [email, setEmail] = useState("")
+  const [username, setUsername] = useState("")
+  const [password, setPassword] = useState("")
+  const [sessionToken, setSessionToken] = useState<string | null>("");
+  const [role, setRole] = useState('member')
+
+  useEffect(() => {
+    if (localStorage.getItem('token')){
+      setSessionToken(localStorage.getItem('token'))
+    }
+  }, [])
+
+  const clearToken = () => {
+    localStorage.clear();
+    setSessionToken('');
+  }
+
+  const updateToken = (newToken:any) => {
+    localStorage.setItem('token', newToken);
+    setSessionToken(newToken);
+    console.log(sessionToken);
+}
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Signup email={email} setEmail={setEmail}
+               username= {username} setUsername={setUsername}
+               password= {password} setPassword={setPassword} 
+               updateToken = {updateToken}
+               />
     </div>
   );
 }
---
+++ src/components/Signup.tsx
@@ -0,0 +1,79 @@
+import React, { Component, useState } from "react";
+import {Form, FormGroup, Label, Input, Button} from 'reactstrap';
+import * as Yup from "yup";
+
+interface SignupProps {
+email: string,
+username: string,
+password: string,
+setEmail: (e:string)=> void,
+setUsername: (e:string) => void,
+setPassword: (e:string) => void,
+updateToken: (newToken:any) => void
+
+
+    
+
+
+
+                                //props, state
+class Signup extends Component <SignupProps,{}> {  
+    constructor(props: any) {
+        super(props)
+        
+        const [role, setRole] = useState('member')
+
+        
+    }
+    handleSubmit = (e:any) => {
+       e.preventDefault();
+
+       fetch("http://localhost:5000/user/signup", {
+           method: 'POST',
+           headers: new Headers({
+               'Content-Type': 'application/json'
+           }),
+           body: JSON.stringify({user:{email:this.props.email, username:this.props.username ,password: this.props.password, 
+                                    role:'member'}}),
+       }).then(
+           (response) => response.json()
+       ).then((data) => { 
+           this.props.updateToken(data.sessionToken)    
+
+       })
+       .catch(err =>
+           console.log(err))
+   }
+
+    render(): React.ReactNode {
+        return (
+            <div>
+                <h1>Sign Up</h1>
+                <Form onSubmit={this.handleSubmit}>
+                    
+                <FormGroup>
+                    <Label htmlFor="username">Username</Label>
+                    <Input onChange={(e) => this.props.setUsername(e.target.value)} name="username" value={this.props.username} />
+                </FormGroup>
+                <FormGroup>
+                    <Label htmlFor="email">Email</Label>
+                    <Input onChange={(e) => this.props.setEmail(e.target.value)} name="email" value={this.props.username} />
+                </FormGroup>
+                <FormGroup>
+                    <Label htmlFor="password">Password</Label>
+                    <Input onChange={(e) => this.props.setPassword(e.target.value)} name="password" value={this.props.password} />
+                </FormGroup>
+                <FormGroup>
+                    <Label htmlFor="role">Role</Label>
+                    <Input name='role' value={role}  ></Input>
+                </FormGroup>
+                <Button type="submit">Sign Up</Button>
+
+                </Form>
+
+            </div>
+        )
+    }
+}
+
+export default Signup;
